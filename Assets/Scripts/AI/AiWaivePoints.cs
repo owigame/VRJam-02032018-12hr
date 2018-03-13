ead6a1a21cc1f7da73cd2fa5920e85f421c0b0e4
RaptorTech4 <bennet.rumboll@gmail.com>
2018-03-13 15:56:53 +0200

    ai movement

---
+++ Assets/Scripts/AI/AiWaivePoints.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiWaivePoints : MonoBehaviour {
+
+    public static Transform[] MovePoints;
+
+    private void Awake()
+    {
+        MovePoints = new Transform[transform.childCount];
+
+        for (int i = 0; i < MovePoints.Length; i++)
+        {
+            MovePoints[i] = transform.GetChild(i);
+        }
+
+    }
+}
