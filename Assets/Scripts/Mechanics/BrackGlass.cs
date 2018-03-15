c757b33a6b3493db0b4cb2c8e4cfa26a8e302dff
RaptorTech4 <bennet.rumboll@gmail.com>
2018-03-15 10:11:19 +0200

    glass update + script

---
+++ Assets/Scripts/Mechanics/BrackGlass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrackGlass : MonoBehaviour
+{
+    public Transform BrokenGlass;
+    public float objectMoveToBrakeSpeed;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > objectMoveToBrakeSpeed)
+        {
+            Destroy(gameObject);
+            Instantiate(BrokenGlass, transform.position, transform.rotation);
+            BrokenGlass.localScale = transform.localScale;
+        }
+    }
+
+}
